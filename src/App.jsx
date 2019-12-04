311896586142770f62169614aa60a44325d2c7c4
Kirill Shamanin <kirill.shamanin@mewssystems.com>
2019-12-04 14:46:13 +0100

    added some cosmetics

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import TodoList from './components/Todo/TodoList';
 import Form from './components/AddTodo/Form';
+import { Grid } from '@material-ui/core';
 
 const App = () => {
-  return (
-    <div>
-      <Form />
-      <TodoList />
-    </div>
-  );
-}
+    return (
+        <Grid container alignItems="center" justify="center" xs={12}>
+            <Grid direction="column"c container xs={6} spacing={2}>
+                <Grid item>
+                    <Form />
+                </Grid>
+                <Grid item>
+                    <TodoList />
+                </Grid>
+            </Grid>
+        </Grid>
+    );
+};
 
 export default App;
