e0b12c95e98ae385606c3403fb062abcd779c9de
Kirill Shamanin <kirill.shamanin@mewssystems.com>
2019-12-04 12:58:53 +0100

    added redux actions/reducers

--- src/store/index.js
+++ src/store/index.js
@@ -1,1 +1,11 @@
-export {};
+import { combineReducers } from 'redux';
+import { todoReducer } from './reducer';
+import { createStore } from 'redux';
+
+const todoApp = combineReducers({
+    todoReducer,
+});
+
+const store = createStore(todoApp);
+
+export default store;
